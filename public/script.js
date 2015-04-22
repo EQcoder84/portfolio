5749db69e3dc2cd1c1703b906891611194ae1d21
Enrique Quevedo <efqueved@aol.com>
2015-04-22 16:35:18 -0400

    updated html and css for portfolio site

---
+++ public/script.js
@@ -0,0 +1,14 @@
+$(document).ready(function () {
+$('.grow').mouseenter(function() {
+        $('.grow').fadeTo('fast', 1);
+});
+    
+    $('div').mouseleave(function() {
+        $('.grow').fadeTo('fast', 0.5);
+    });
+ });
+
+
+
+
+
